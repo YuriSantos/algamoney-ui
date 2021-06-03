dee4cf1b96eaa002642b2142812eaf2916e9b670
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-06-03 12:25:11 -0300

    Criando o componente de cadastro de pessoas - Aula 13.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,12 @@ import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+<<<<<<< Updated upstream
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
+=======
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -24,7 +28,11 @@ import { CurrencyMaskModule } from 'ng2-currency-mask';
     LancamentosPesquisaComponent,
     NavBarComponent,
     PessoasPesquisaComponent,
+<<<<<<< Updated upstream
     LancamentoCadastroComponent
+=======
+    PessoaCadastroComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
