7c9c03cda3da28a9f31c050eee445c81d07e3d28
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-06-19 19:24:53 -0300

    Adicionando o serviço de pesquisa de pessoas pela API - Aula 17.7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
