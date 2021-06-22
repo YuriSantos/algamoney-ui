6111c457f6de172559a826777062b5b5707352e9
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-06-22 19:40:34 -0300

    Implementando rotas - Aula 18.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 
 
+const routes: Routes = [
+  {path: 'lancamentos', component: LancamentosPesquisaComponent},
+  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  {path: 'pessoas', component: PessoasPesquisaComponent},
+  {path: 'pessoas/novo', component: PessoaCadastroComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,6 +30,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
 
     CoreModule,
     LancamentosModule,
