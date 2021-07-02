129921674c9a3483646481d4763f452947514d4e
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-07-02 19:15:23 -0300

    Corrigindo o JWT

--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -7,6 +7,7 @@ import { ButtonModule } from 'primeng/button';
 import { SegurancaRoutingModule } from './seguranca-routing.module';
 import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
 import { AuthGuard } from './auth.guard';
+import { environment } from 'src/environments/environment';
 
 export function tokenGetter(): string {
   return localStorage.getItem('token');
@@ -24,8 +25,8 @@ export function tokenGetter(): string {
     JwtModule.forRoot({
       config: {
         tokenGetter,
-        allowedDomains: ['localhost:8080'],
-        disallowedRoutes: ['http://localhost:8080/oauth/token']
+        allowedDomains: environment.tokenAllowedDomains,
+        disallowedRoutes: environment.tokenDisallowedRoutes
       }
     }),
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://algamoneyjp-api.herokuapp.com'
+  apiUrl: 'https://algamoneyjp-api.herokuapp.com',
+  tokenAllowedDomains: [ /algamoneyui-angular.herokuapp.com/ ],
+  tokenDisallowedRoutes: [/\/oauth\/token/]
 };
