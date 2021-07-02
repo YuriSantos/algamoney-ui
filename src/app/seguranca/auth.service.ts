9a2a806ed9f6e8fa4bf48d0dca07dac822a72384
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-07-02 08:29:03 -0300

    Implementando a interceptação de chamadas HTTP com expiração do token - Aula 19.11

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -40,6 +40,35 @@ export class AuthService {
       });
   }
 
+  obterNovoAccessToken(): Promise<void> {
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/x-www-form-urlencoded')
+      .append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
+
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post(this.oauthTokenUrl, body,
+        { headers, withCredentials: true })
+      .toPromise()
+      .then(response => {
+        this.armazenarToken(response['access_token']);
+
+        console.log('Novo access token criado!');
+
+        return Promise.resolve(null);
+      })
+      .catch(response => {
+        console.error('Erro ao renovar token.', response);
+        return Promise.resolve(null);
+      });
+  }
+
+  isAccessTokenInvalido() {
+    const token = localStorage.getItem('token');
+
+    return !token || this.jwtHelper.isTokenExpired(token);
+  }
+
   temPermissao(permissao: string) {
     return this.jwtPayload && this.jwtPayload.authorities.includes(permissao);
   }
