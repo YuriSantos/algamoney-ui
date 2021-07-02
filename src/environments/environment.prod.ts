71720336b6b5f43b6a6790e2ce5ddd678728408d
Yuri Barbosa Santos <yurisantosjp@gmail.com>
2021-07-02 19:34:07 -0300

    Corrigindo o token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://algamoneyjp-api.herokuapp.com',
-  tokenAllowedDomains: [ /algamoneyui-angular.herokuapp.com/ ],
-  tokenDisallowedRoutes: [/\/oauth\/token/]
+  tokenAllowedDomains: [ new RegExp ('/algamoneyui-angular.herokuapp.com/') ],
+  tokenDisallowedRoutes: [ new RegExp ('/\/oauth\/token/')]
 };
